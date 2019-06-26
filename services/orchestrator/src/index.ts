21a4ebadcca7084c880f0f04c67491298d978676
R. Tyler Croy <rtyler@brokenco.de>
2019-06-25 20:44:59 -0700

    Bootstrap the orchestration service from a common configuration

--- services/orchestrator/src/index.ts
+++ services/orchestrator/src/index.ts
@@ -3,4 +3,20 @@ require('module-alias/register');
 import logger from '@otto/logger';
 import { serveApp } from '@otto/server'
 
+import feathers from '@feathersjs/feathers';
+import configuration from '@feathersjs/configuration';
+import express from '@feathersjs/express';
+
 logger.info('Starting orchestrator');
+
+const app = express(feathers());
+
+// We need to fish out the orchestrator specific settings
+const settings = configuration()(app);
+app.configure((app) => {
+  Object.keys(settings['orchestrator']).forEach((key) => {
+    app.set(key, settings['orchestrator'][key]);
+  });
+});
+
+serveApp(app);
