5bdd7b6c742c42e5f20be85e475f203c996c2d32
R. Tyler Croy <rtyler@brokenco.de>
2019-07-06 20:35:25 -0700

    Finish up some testing of the configure block

--- grammar/__tests__/configure.ts
+++ grammar/__tests__/configure.ts
@@ -12,4 +12,19 @@ describe('configure {}', () => {
   it('should pass with an empty configure block', () => {
     expect(parse(`configure {} ${MIN_PIPELINE}`)).toHaveLength(0);
   });
+
+  it('should pass on an empty settings block within the configure {}', () => {
+    expect(parse(`configure { github {} } ${MIN_PIPELINE}`)).toHaveLength(0);
+  });
+
+  it('should fail on a missing block', () => {
+    expect(parse(`configure { github } ${MIN_PIPELINE}`)).not.toHaveLength(0);
+  });
+
+  it('should pass on a settings block within the configure {}', () => {
+    expect(parse(`configure { github { account = 'rtyler' } } ${MIN_PIPELINE}`)).toHaveLength(0);
+  });
+  it('should pass on a many settings within the configure {}', () => {
+    expect(parse(`configure { github { account = 'rtyler' endpoint = 'api.github.com' } } ${MIN_PIPELINE}`)).toHaveLength(0);
+  });
 });
