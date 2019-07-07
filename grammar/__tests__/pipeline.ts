42a10e3b9b0a0a5f20ffdc70bb502c48db272a40
R. Tyler Croy <rtyler@brokenco.de>
2019-07-07 09:00:31 -0700

    Automatically test parsing of all the .otto examples in the examples/ directory

--- grammar/__tests__/pipeline.ts
+++ grammar/__tests__/pipeline.ts
@@ -2,10 +2,23 @@
  * This test file will verify the parsing behavior of the pipeline block
  */
 
+import fs from 'fs';
+import path from 'path';
+
 import { MIN_PIPELINE, parse } from '../utils';
 
 describe('pipeline {}', () => {
   it('should pass with the minimum viable pipeline', () => {
     expect(parse(MIN_PIPELINE)).toHaveLength(0);
+  });
+
+  describe('with full examples', () => {
+    const examples_dir = path.join(__dirname, '..', '..', 'examples');
+    fs.readdirSync(examples_dir).filter(filename => filename.endsWith('.otto')).map((filename) => {
+      it(`should be able to parse ${filename}`, () => {
+        const buffer = fs.readFileSync(path.join(examples_dir, filename), 'utf8');
+        expect(parse(buffer)).toHaveLength(0);
+      });
+    });
   });
 });
