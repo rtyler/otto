fcda970dddca578be95c95eb0c73abf4db83df3c
R. Tyler Croy <rtyler@brokenco.de>
2019-12-30 15:42:28 -0800

    Play around with workers handling messages in the auctioneer
    This seems to spin in a sched_yield loop, which I think is the ctx.recv().await?

--- auctioneer/src/main.rs
+++ auctioneer/src/main.rs
@@ -1,12 +1,17 @@
 extern crate bastion;
+extern crate num_cpus;
 extern crate pretty_env_logger;
 extern crate tungstenite;
 extern crate url;
 
+use std::time::Duration;
+use std::thread::sleep;
+use std::sync::Arc;
 use bastion::prelude::*;
 use tungstenite::{connect, Message};
 use url::Url;
 
+use log::*;
 use otto_eventbus::Command;
 
 fn main() {
@@ -16,38 +21,64 @@ fn main() {
     // ...and initializing the system with it (this is required)...
     Bastion::init_with(config);
 
-    // Note that `Bastion::init();` would work too and initialize
-    // the system with the default config.
-
-
     let supervisor = Bastion::supervisor(|sp| {
         sp.with_strategy(SupervisionStrategy::OneForOne)
     }).expect("Couldn't create the supervisor.");
 
+    let workers = Bastion::children(|children: Children| {
+        children
+            .with_redundancy(num_cpus::get())
+            .with_exec(move |ctx: BastionContext| {
+                async move {
+                    info!("Starting child {:?}", ctx.current().id());
+                    loop {
+                        msg! { ctx.recv().await?,
+                            msg: Message => {
+                                info!("Received message {} on {:?}", msg, ctx.current().id());
+                            };
+                            _: _ => {
+                                ()
+                            };
+                        }
+                    }
+                    Ok(())
+                }
+            })
+
+    }).expect("Couldn't start workers group");
+
+    let workers = Arc::new(workers);
+
     Bastion::children(|children: Children| {
         children.with_exec(move |ctx: BastionContext| {
+            let workers = workers.clone();
             async move {
-                println!("Server is starting!");
+                info!("Server is starting!");
 
                 let (mut socket, response) =
-                    connect(Url::parse("ws://localhost:8000/ws/").unwrap()).expect("Can't connect");
+                    connect(Url::parse("ws://localhost:8000/ws/").unwrap())
+                    .map_err(|e| {
+                        error!("Failed to connect!");
+                        sleep(Duration::from_secs(1));
+                    }).expect("Can't connect");
 
-                println!("Connected to the server");
-                println!("Response HTTP code: {}", response.code);
-                println!("Response contains the following headers:");
+                info!("Response HTTP code: {}", response.code);
+                info!("Response contains the following headers:");
                 for (ref header, _value) in response.headers.iter() {
-                    println!("* {}", header);
+                    info!("* {}", header);
                 }
 
                 socket
                     .write_message(Message::Text("Hello WebSocket".into()))
                     .unwrap();
-                loop {
-                    let msg = socket.read_message().expect("Error reading message");
-                    println!("Received: {}", msg);
-                }
-                // socket.close(None);
 
+                loop {
+                    for worker in workers.elems().iter() {
+                        let msg = socket.read_message().expect("Error reading message");
+                        info!("telling {:?}", worker.id());
+                        worker.tell(msg);
+                    }
+                }
                 // Send a signal to system that computation is finished.
                 Bastion::stop();
 
@@ -56,7 +87,6 @@ fn main() {
         })
     })
     .expect("Couldn't start a new children group.");
-
 
     // Starting the system...
     Bastion::start();
