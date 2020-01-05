e67165f10bfa569b90af6d5046621f6f403b030c
R. Tyler Croy <rtyler@brokenco.de>
2020-01-04 22:39:05 -0800

    Introduce rustyline for the eventbus CLI

--- eventbus/src/cli.rs
+++ eventbus/src/cli.rs
@@ -1,7 +1,12 @@
 /**
  * The CLI is meant to be used for manual testing and verification of the eventbus only.
  */
+
+extern crate rustyline;
+
 use std::io::{stdin, stdout, Write};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use tungstenite::*;
 use url::Url;
 
@@ -10,18 +15,38 @@ fn main() {
         connect(Url::parse("ws://localhost:8000/ws/").unwrap()).expect("Failed to connect");
     println!("Connected to the server");
 
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    let history = ".otto-ebc-history";
+
+    if rl.load_history(history).is_err() {
+        println!("No previous history");
+    }
+
     loop {
-        let mut message = String::new();
-        print!("> ");
-        let _ = stdout().flush();
-
-        let _ = stdin().read_line(&mut message).unwrap();
-
-        if let Some('\n') = message.chars().next() {
-            let msg = socket.read_message().expect("Failed to read message");
-            println!("Received: {}", msg);
-        } else {
-            socket.write_message(Message::Text(message));
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                if line.len() == 0 {
+                    let msg = socket.read_message().expect("Failed to read message");
+                    println!("Received: {}", msg);
+                } else {
+                    socket.write_message(Message::Text(line));
+                }
+            },
+            Err(ReadlineError::Interrupted) => {
+                // ctrl-C
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                // ctrl-D
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
         }
     }
+    rl.save_history(history).unwrap();
 }
