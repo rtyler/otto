f347234c0595721bbf4bba4bc9bdbda1f6694664
R. Tyler Croy <rtyler@brokenco.de>
2020-01-04 20:05:39 -0800

    Properly handle server disconnects in the auctioneer
    This at least ensures that we're reconnecting and can continue to receive
    messages when the server disappears

--- auctioneer/src/main.rs
+++ auctioneer/src/main.rs
@@ -9,7 +9,9 @@ use std::{io, thread};
 
 use actix::*;
 use log::*;
-use tungstenite::{connect, Message};
+use tungstenite::client::AutoStream;
+use tungstenite::handshake::client::Response;
+use tungstenite::*;
 use url::Url;
 
 use otto_eventbus::*;
@@ -24,25 +26,45 @@ impl Actor for BusClient {
     }
 }
 
+/**
+ * Simple websocket connection function, just to allow for easy reconnections
+ */
+fn ws_connect() -> Result<(WebSocket<AutoStream>, Response)> {
+    return connect(Url::parse("ws://localhost:8000/ws/").unwrap());
+}
+
 #[actix_rt::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let (mut socket, response) =
-        connect(Url::parse("ws://localhost:8000/ws/").unwrap()).expect("Can't connect");
+    let (mut socket, _response) = ws_connect().unwrap();
 
-    println!("Connected to the server");
-
-    //socket
-    //    .write_message(Message::Text("Hello WebSocket".into()))
-    //    .unwrap();
+    info!("Connected to the server");
 
     /*
      * In this loop we should read the message, and then dispatch it to a handler actor immediately
      * to do "the work"
      */
     loop {
-        let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
+        let msg = socket.read_message();
+        let msg = match msg {
+            Ok(m) => m,
+            Err(e) => {
+                error!("Failed to read a message off the WebSocket: {:?}", e);
+                // must reconnect
+                thread::sleep(Duration::from_secs(3));
+
+                /*
+                 * Ignore connection errors, they're not important since we
+                 * are in the retry loop anyways.
+                 */
+                if let Ok(recon) = ws_connect() {
+                    info!("Server reconnected");
+                    socket = recon.0;
+                }
+                continue;
+            }
+        };
+        info!("Received: {}", msg);
     }
 }
