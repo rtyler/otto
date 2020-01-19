906aa2bc658134a0a1596406096cdaa093dec761
R. Tyler Croy <rtyler@brokenco.de>
2020-01-19 10:53:16 -0800

    Does not compile: experimenting with APIs for the Eventbus
    This is just some tinkering I wish to checkpoint before ripping it all out again

--- eventbus/src/lib.rs
+++ eventbus/src/lib.rs
@@ -7,7 +7,47 @@
 pub mod client;
 pub mod msg;
 
-trait Bus {}
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::broadcast::{channel, Receiver, Sender};
+
+/**
+ * A channel is named and typed with the type of messages it should be carrying
+ */
+struct ChannelV<T> {
+    name: String,
+    send: Sender<T>,
+    recv: Receiver<T>,
+}
+
+struct StringChannel {
+    name: String,
+    send: Sender<String>,
+    recv: Receiver<String>,
+}
+struct SM(String);
+impl Message for SM {}
+
+impl Channel<SM> for StringChannel {
+    fn send(&self, msg: SM) {}
+    fn recv(&self, msg: SM) {}
+}
+
+trait Message {}
+
+trait Channel<T> {
+    fn send(&self, msg: impl Message);
+    fn recv(&self, msg: impl Message);
+}
+
+struct Bus {
+    /**
+     * Channels are named and can implement a number of different types. This should
+     * allow the Bus to handle different channels with different message payloads
+     * while still taking advantage of compile-time checks
+     */
+    channels: HashMap<String, Box<Channel<Message>>>,
+}
 
 #[cfg(test)]
 mod tests {
