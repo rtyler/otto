5048c04bf9ff893d1d98169734253b8d0788f6c5
R. Tyler Croy <rtyler@brokenco.de>
2020-10-17 14:53:57 -0700

    Implement the osp tool, something simple to package a step library into a tarball

---
+++ osp/src/lib.rs
@@ -0,0 +1,80 @@
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Manifest {
+    symbol: String,
+    description: String,
+    includes: Vec<Include>,
+    entrypoint: Entrypoint,
+    parameters: Vec<Parameter>,
+}
+
+impl Manifest {
+    /**
+     * Create an artifact from the given manifest
+     */
+    pub fn create_artifact(&self, dir: &Path, output: &Path) -> Result<(), std::io::Error> {
+        let tar_gz = File::create(output)?;
+        let enc = GzEncoder::new(tar_gz, Compression::default());
+        let mut tar = tar::Builder::new(enc);
+        let mut manifest = File::open(dir.join(Path::new("manifest.yml")))?;
+        tar.append_file(format!("{}/manifest.yml", self.symbol), &mut manifest)?;
+
+        for include in self.includes.iter() {
+            let mut f = File::open(&include.name)?;
+            let archive_path = format!("{}/{}",
+                                    self.symbol,
+                                    match include.flatten {
+                                        true => {
+                                            let p = Path::new(&include.name);
+                                            p.file_name().unwrap().to_str().unwrap()
+                                        },
+                                        false => &include.name,
+                                    });
+            tar.append_file(archive_path, &mut f);
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Include {
+    name: String,
+    #[serde(default = "default_false")]
+    flatten: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Entrypoint {
+    path: PathBuf,
+    #[serde(default = "default_false")]
+    multiarch: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Parameter {
+    name: String,
+    required: bool,
+    #[serde(rename = "type")]
+    p_type: ParameterType,
+    description: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum ParameterType {
+    #[serde(rename = "string")]
+    StringParameter,
+    #[serde(rename = "boolean")]
+    BoolParameter,
+}
+
+/** Simple function for serde defaults */
+fn default_false() -> bool { false }
+
+#[cfg(test)]
+mod tests {
+}
