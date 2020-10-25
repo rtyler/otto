f7436c111e5c5d0386d973022de543d2d744d360
R. Tyler Croy <rtyler@brokenco.de>
2020-10-25 14:28:42 -0700

    Receive control requests, not currently doing anything with them however.
    Tested with:
        ❯ curl --unix-socket $PWD/agent.sock -X POST -d '{"type":"Terminate"}' http://agent/control
        {}

--- agent/src/control.rs
+++ agent/src/control.rs
@@ -11,12 +11,19 @@ enum Request {
     Terminate,
 }
 
+async fn handle_request(mut req: tide::Request<()>) -> tide::Result {
+    let message: Request = req.body_json().await?;
+    debug!("Handling control request: {:#?}", message);
+    Ok("{}".into())
+}
+
 pub async fn run() -> tide::Result<()> {
     info!("Starting the agent control server");
     let sock = "agent.sock";
     let mut app = tide::new();
 
     app.at("/").get(|_| async { Ok(format!("Otto Agent v{}", env!["CARGO_PKG_VERSION"])) });
+    app.at("/control").post(handle_request);
 
     if let Err(e) = std::fs::remove_file(&sock) {
         warn!("Failed while trying to remove any previous {}, this might be okay", &sock);
