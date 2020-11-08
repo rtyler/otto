b70cf8c17be084610f2f3103334fd56381eeac78
R. Tyler Croy <rtyler@brokenco.de>
2020-11-08 15:01:06 -0800

    Update the object-store to also allow use with catflap

--- services/object-store/src/main.rs
+++ services/object-store/src/main.rs
@@ -2,11 +2,17 @@ use std::path::Path;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
-    pretty_env_logger::init();
+    use std::{env, net::TcpListener, os::unix::io::FromRawFd};
+    tide::log::start();
 
     let upload_dir = std::env::var("OTTO_OBJECT_DIR").unwrap_or("tmp".to_string());
-    otto_objectstore::app(Path::new(&upload_dir).to_path_buf())
-        .listen("127.0.0.1:7671")
-        .await?;
+    let app = otto_objectstore::app(Path::new(&upload_dir).to_path_buf());
+
+    if let Some(fd) = env::var("LISTEN_FD").ok().and_then(|fd| fd.parse().ok()) {
+        app.listen(unsafe { TcpListener::from_raw_fd(fd) }).await?;
+    }
+    else {
+        app.listen("http://localhost:7671").await?;
+    }
     Ok(())
 }
