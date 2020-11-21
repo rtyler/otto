391894ded7cfe88d8d96eadabdb3bd2693329890
R. Tyler Croy <rtyler@brokenco.de>
2020-11-21 15:42:10 -0800

    Add the local orchestrator apispecs and a simple healthcheck
    Still not sure what this API should look like, but will start playing around
    with it.

--- services/local-orchestrator/src/main.rs
+++ services/local-orchestrator/src/main.rs
@@ -1,13 +1,22 @@
 /*
  * The local orchestrator doesn't do much
  */
+use tide::Request;
+
+async fn healthcheck(_req: Request<()>) -> tide::Result {
+    Ok(tide::Response::builder(200)
+        .body("{}")
+        .content_type("application/json")
+        .build())
+}
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
     use std::{env, net::TcpListener, os::unix::io::FromRawFd};
     tide::log::start();
 
-    let app = tide::new();
+    let mut app = tide::new();
+    app.at("/health").get(healthcheck);
 
     if let Some(fd) = env::var("LISTEN_FD").ok().and_then(|fd| fd.parse().ok()) {
         app.listen(unsafe { TcpListener::from_raw_fd(fd) }).await?;
